bc77f96a92d553b5d73eb8f6fb70a4259561405b
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2024-12-19 23:00:21 +0100

    step1 erroring

--- steps/step1.py
+++ steps/step1.py
@@ -4,3 +4,18 @@ from tqdm import tqdm
 import logging
 import PyPDF2
 import os
+
+logger = logging.getLogger(__name__)
+
+class PDFProcessingError(Exception):
+    """Base exception for PDF processing errors."""
+    pass
+class PDFValidationError(PDFProcessingError):
+    """Exception raised for PDF validation issues."""
+    pass
+class PDFExtractionError(PDFProcessingError):
+    """Exception raised for text extraction issues."""
+    pass
+class ChunkProcessingError(PDFProcessingError):
+    """Exception raised for chunk processing issues."""
+    pass
